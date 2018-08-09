7404de4114cc3a0d2f454dadfbfbbcc5eac9924b
pglotfel <pglotfel@gmail.com>
2018-08-09 18:05:27 +0000

    updates

--- gritsbot/gritsbot.py
+++ gritsbot/gritsbot.py
@@ -3,38 +3,38 @@ import json
 import vizier.mqttinterface as mi
 import concurrent.futures as futures
 import queue
+import time
 
 
 s = serial.Serial('/dev/ttyACM0', 9600)
-m = mi.MQTTInterface(host='192.168.1.12', port=1884)
+#m = mi.MQTTInterface(host='192.168.1.12', port=1884)
 e = futures.ThreadPoolExecutor()
 
-# Proposed packet structure
-request = {'method': 'read', 'body': 'voltage'}
-packet = {'method': 'write', 'body': {}}
+# 0 -- read 
+# 1 -- write
 
-response = {'status': 1, 'body': {}}
+# Proposed request packet structure
+request = {'method': 'write', 'body': {'type': 'bat_volt'}}
+packet = {'method': 'read', 'body': {'v': 0, 'w': 0, 'type': 'motor'}}
 
+# Proposed response packet structure
+response = {'status': 1, 'body': {'bat_volt': 4.3}}
 
-def write_job():
-    while True:
-        to_write = write_q.get()
-        if(to_write):
-            s.write(to_write()) 
-        else:
-            break
+def create_request(method, body):
+    return {'method': method, 'body': body}
 
-def read_job():
-    while True:
-        # Check if closed.  If closed, then exit the loop.  Probably need try/catch here as well
-        to_read = s.read(s.inWaiting()).decode('ASCII')
-        read_q.put(to_read)
+def json_to_bytes(message):
+    return json.dumps(message).encode('ASCII') 
 
-def send_angular_velocities(wl, wr):
+def bytes_to_json(message):
+    return json.loads(message.decode('ASCII'))
+
+def send_angular_velocities(v, w):
 
     # Doesn't require a handshake
     global s
-    s.write(json.dumps({'wl': wl, 'wr': wr}).encode('ASCII'))
+    msg = create_request('write', {'v': float(v), 'w': float(w), 'type': 'motor'})
+    s.write(json_to_bytes(msg))
 
 def read_battery_voltage():
     
@@ -42,19 +42,32 @@ def read_battery_voltage():
     # read from serial
     def serial_read():
         global s
-        # TODO: Check if this returns immediately if inWaiitng = 0
-        return s.read(s.inWaiting())
+        # Read to wait for bytes to be available
+        msg = s.read()
+        # Once bytes are available, read the rest in.  We assume that the entire message is on 
+        # the line
+        msg += s.read(s.in_waiting)
+        return bytes_to_json(msg)
 
     future_serial_read = e.submit(serial_read) 
-    s.write() # Write motor packet
-    return future_serial_read.result()
+    msg = create_request('read', {'type': 'bat_volt'})
+    s.write(json_to_bytes(msg)) # Write request packet
+
+    # The result of the future should be in the appropriate format
+    result = future_serial_read.result()
+    if('body' in result):
+        print('Got result: {}'.format(result['body']))
+    else:
+        # Should be an error
+        pass
 
 def main():
-
-    read_battery_voltage()
-    send_angular_velocities()
-    
-
+    start = 0
+    while True:
+        read_battery_voltage()
+        send_angular_velocities(start, 0.0)
+        time.sleep(0.5)
+        start = (start + 1) % 26
 
 if __name__ == '__main__': 
     main()

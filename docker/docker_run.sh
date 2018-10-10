2eaed43410dcf64603f870b5723e7864dc10668f
pglotfel <pglotfel@gmail.com>
2018-10-10 13:21:39 -0400

    name container

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -2,6 +2,7 @@
 
 # Use the detect_serial module to get the correct serial port
 docker run -d --restart=always \
+	--name firmware \
 	--net host \
 	--device $(python3 -m gritsbot.utils.detect_serial):/dev/ttyACM0 \
 	robotarium:firmware \

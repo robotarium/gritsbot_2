36bb3930c43acc665cab3083d7c7f85f37db76e3
pglotfel <pglotfel@gmail.com>
2018-10-30 17:10:36 -0400

    minor change

--- docker/base/docker_watch.sh
+++ docker/base/docker_watch.sh
@@ -3,4 +3,4 @@
 docker run -d \
 	--name watchtower_firmware \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower:armhf-latest robotarium/firmware -i 60 --debug 
+	v2tec/watchtower:armhf-latest -i 60 --debug firmware

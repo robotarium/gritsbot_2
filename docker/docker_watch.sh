4e34ac3d27feb0a239f91ef7e2435089a8bdcaae
pglotfel <pglotfel@gmail.com>
2018-10-30 19:05:09 -0400

    watch everything

--- docker/docker_watch.sh
+++ docker/docker_watch.sh
@@ -2,6 +2,6 @@
 
 docker run -d \
 	--restart always \
-	--name watchtower_firmware \
+	--name watchtower \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower:armhf-latest -i 60 --debug firmware
+	v2tec/watchtower:armhf-latest -i 60 --debug

72e8597a499de5b8af16526c3d23c5d48b4123f8
pglotfel <pglotfel@gmail.com>
2018-10-30 16:57:52 -0400

    debug

--- docker/base/docker_watch.sh
+++ docker/base/docker_watch.sh
@@ -3,4 +3,4 @@
 docker run -d \
 	--name watchtower_firmware \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower:armhf-latest -i 60 robotarium/firmware
+	v2tec/watchtower:armhf-latest -i 60 --debug robotarium/firmware

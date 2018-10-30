eba58371484bb55ba826aa7651eb2c919cc44000
pglotfel <pglotfel@gmail.com>
2018-10-30 19:16:48 -0400

    add label

--- docker/docker_watch.sh
+++ docker/docker_watch.sh
@@ -3,6 +3,6 @@
 docker run -d \
 	--label=com.centurylinklabs.watchtower.enable=false \
 	--restart always \
-	--name watchtower \
+	--name watchtower_firmware \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	v2tec/watchtower:armhf-latest -i 60 --debug firmware
